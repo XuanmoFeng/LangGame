a01375da59fe717f780e1e56c1bf5305f76bff33
XuanmoFeng <763608087@qq.com>
2017-08-24 20:45:26 +0800

    数据表的python创建

--- SqlLang.py
+++ SqlLang.py
@@ -13,67 +13,30 @@ def TRS(name):
         return '女巫'
     elif name =='Civilians':
         return '平民'
-
-#def CreateData():
+#def CreateTableLANG_PLAYER():#调用直接生成LANG_PLAYER表格
 #    db=MySQLdb.connect("localhost","root","123456","db_Lang")
 #    cursor=db.cursor()
-#    sql="""CREATE TABLE HOME (
-#        HOMEID INT(4) NOT NULL,
-#        TOTAL INT NOT NULL,
-#        EXTRAL INT NOT NULL,
-#        USERID CHAR(20) NOT NULL,
-#        PRIMARY KEY(HOMEID)
+#    sql="""CREATE TABLE LANG_PLAY (
+#        NUM INT(4) NOT NULL,
+#        CONUT CHAR(20) NOT NULL,
+#        EXTRAL CHAR(20) NOT NULL,
+#        USERID INT NOT NULL,
 #        ) """
 #    cursor.execute(sql)
 #    db.close()
-#def CreateTable():
+
+#def CreateTablePALY():#调用直接生成PALY表格
 #    db=MySQLdb.connect("localhost","root","123456","db_Lang")
 #    cursor=db.cursor()
 #    sql="""CREATE TABLE PLAY (
 #        HOMEID INT(4) NOT NULL,
 #        PALYNAME CHAR(20) NOT NULL,
 #        USERID CHAR(20) NOT NULL,
-#        FLAG INT NOT NULL
+#        FLAG INT NOT NULL,
+#        usernum INT(4) NOT NULL
 #        ) """
 #    cursor.execute(sql)
 #    db.close()
-def join(da):
-    return da
-#def Join(homeid):
-#    db=MySQLdb.connect("localhost","root","123456","db_Lang")
-#    cursor=db.cursor()
-#    sql ="SELECT HOMEID FROM HOME WHERE HOMEID='%d'"%(homeid)
-#    cursor.execute(sql)
-#    results=cursor.fetchall()
-#    if results:
-#        sql ="SELECT TOTAL,EXTRAL FROM HOME WHERE HOMEID='%d'"%(homeid)
-#        cursor.execute(sql)
-#        results=cursor.fetchall()
-#        num=results[0][1]
-#        total=results[0][0]
-#        if num==0:
-#            return "房间人数已满"
-#        else:
-#            n=num-1
-#            sql="update HOME set EXTRAL='%d' WHERE HOMEID='%d'"%(n,homeid)
-#            cursor.execute(sql)
-#            db.commit()
-#            while 1:
-#                n=random.randint(0,total)
-#                sql ="SELECT PALYNAME FROM PLAY WHERE usenum='%d'and HOMEID='%d'and FLAG='1'"%(n,homeid)
-#                cursor.execute(sql)
-#                results=cursor.fetchall()
-#                if results:
-#                    sql="update PLAY set FLAG='0' WHERE HOMEID='%d' and usenum='%d'"%(homeid,n)
-#                    cursor.execute(sql)
-#                    db.commit()
-#                    str="房间号 %d"%homeid+'\r\n'+"你的身份 "+TRS(results[0][0])+'\r\n'+'你的号码 %d'%n
-#                    db.close()
-#                    return str
-#
-#    else:
-#        db.close()
-#        return  "房间号不存在，请创建房间"
 def Inser(userid,num):
     db=MySQLdb.connect("localhost","root","123456","db_Lang")
     cursor=db.cursor()
